1c34dfcee214c1559f75a2bf5888f1ee4ef902ce
Thamer26 <thamer.alhejailan1@gmail.com>
2018-08-30 17:32:44 +0300

    Create menu_controller_spec.js

---
+++ spec/menu_controller_spec.js
@@ -0,0 +1,14 @@
+const MenuController = require("../controllers/MenuController")
+
+describe("MenuController",()=>{
+	beforeEach(()=>{
+		this.menu = new MenuController();
+	});
+	
+
+	describe("#remindMe()",()=>{
+		it("should return a string 'Learnign is a life-long pursuit'",()=>{
+			expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
+		});
+	});
+});
